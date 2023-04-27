67bf9b730ddb2968e3240ad8ebaac9cdd083150b
muratcan-yuksel <muratcanyukselpro@gmail.com>
2023-04-27 16:11:42 +0300

    add can't be zero error message to number of people part

--- src/components/UserInput.tsx
+++ src/components/UserInput.tsx
@@ -25,17 +25,36 @@ const conditionalTitle = (propType: Props["propType"]) => {
 
 const UserInput = ({ value, propType }: Props) => {
   const billValue = useSelector((state: RootState) => state.bill.value);
+  const peopleValue = useSelector((state: RootState) => state.people.value);
   const dispatch = useDispatch();
+  //give red outline if there are 0 people
+  const outlineClass =
+    propType !== "bill" && peopleValue === 0
+      ? "  outline outline-[#d14721] outline-4"
+      : "";
+
+  const zeroError = (propType: Props["propType"]) => {
+    if (propType !== "bill" && peopleValue === 0) {
+      return "Cant be zero";
+    }
+  };
   return (
-    <div className="flex flex-col items-center">
-      <h2 className="text-[#616D6D] font-bold self-start mb-2 mt-7">
-        {conditionalTitle(propType)}
+    <div className="flex w-full flex-col items-center">
+      <h2 className="w-full text-[#616D6D] font-bold self-start mb-2 mt-7">
+        <div className="flex justify-between  w-full md:max-w-full    ">
+          <div> {conditionalTitle(propType)}</div>
+
+          <div className="ml-12 text-[#d14721]"> {zeroError(propType)}</div>
+        </div>
       </h2>
-      <div className=" bg-[#F3F8FB] flex justify-between items-center  w-full h-14 pl-4 rounded-md mb-7">
+      <div
+        className={` bg-[#F3F8FB] flex justify-between items-center  w-full h-14 pl-4 rounded-md mb-7 ${outlineClass} `}
+      >
         {conditionalImage(propType)}
         <input
           className="bg-[#F3F8FB] appearance-none text-right text-[#0A4A4A] w-full md:max-w-full font-bold text-xl pr-4"
           type="number"
+          placeholder="0"
           onChange={(e) => {
             if (propType === "bill") {
               dispatch(setBill(Number(e.target.value)));
